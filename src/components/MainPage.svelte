d20bbe2221a6c52c309b299a11499d07484bb4bd
Avdo Mesanovic <avdomesanovic@yahoo.com>
2021-01-28 13:10:20 -0600

    changed the columns

--- src/components/MainPage.svelte
+++ src/components/MainPage.svelte
@@ -1,5 +1,5 @@
 <div class="flex-container">
-    <div class="flex-item">1</div>
-    <div class="flex-item">2</div>
-    <div class="flex-item">3</div>
+    <div class="flex-item left">1</div>
+    <div class="flex-item center">2</div>
+    <div class="flex-item right">3</div>
 </div>
