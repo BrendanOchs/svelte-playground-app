b7de1312761ee1f84f94085a23f077b58534936b
romina1127 <diaz-riveror@stlcaps.net>
2021-01-28 12:30:44 -0600

    Some stuff for now (Romina)

--- src/components/MainPage.svelte
+++ src/components/MainPage.svelte
@@ -1,19 +1,23 @@
 <style>
     .flex-container {
         display: flex; /* or inline-flex */
+        align-items: stretch;
+        flex-wrap: wrap;
     }
     
     .flex-item {
         background-color: white;
         border: 2px dashed grey;
         width: 20%;
-        height: 700px;
         margin: 5px;
     }
 
     .flex-item.center{
         flex-grow: 2;
+        min-width: 601px;
     }
+
+    /* width of phone would be about 600px so do a min-width for that I guess */
 
 </style>
 
