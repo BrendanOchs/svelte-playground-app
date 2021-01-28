746d50fb5cdde4ea8ccc95126866a4ffcfa50e3b
Logan Wyas <loganw9999@gmail.com>
2021-01-28 13:29:30 -0600

    Flexbox testing

--- src/components/MainPage.svelte
+++ src/components/MainPage.svelte
@@ -2,5 +2,14 @@
     <div class="flex-item">1</div>
     <div class="flex-item">2</div>
     <div class="flex-item">3</div>
-    <div class="flex-item">3</div>
+    <div class="flex-item">4</div>
+    <div class="flex-item">5</div>
+    <div class="flex-item no-display">6</div>
 </div>
+
+<style>
+    .no-display {
+        opacity: 0;
+        height: 0;
+    }
+</style>
