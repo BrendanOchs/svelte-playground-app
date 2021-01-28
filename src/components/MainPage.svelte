6effd8d22b686dfc115afce535ff6d3835d1aebd
romina1127 <diaz-riveror@stlcaps.net>
2021-01-28 11:58:29 -0600

    Romina's changes so far

--- src/components/MainPage.svelte
+++ src/components/MainPage.svelte
@@ -1,5 +1,16 @@
+<style>
+    .flex-container {
+        display: flex; /* or inline-flex */
+    }
+    
+    .flex-item {
+        width: 30%;
+        height: 700px;
+    }
+</style>
+
 <div class="flex-container">
-    <div class="flex-item">1</div>
-    <div class="flex-item">2</div>
-    <div class="flex-item">3</div>
+    <div class="flex-item left">1</div>
+    <div class="flex-item center">2</div>
+    <div class="flex-item right">3</div>
 </div>
