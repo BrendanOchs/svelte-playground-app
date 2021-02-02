3271be76dbb765ceaabe5fd735472bcd73e906ff
BearN99 <bearn@stlcaps.net>
2021-02-02 11:37:55 -0600

    updating local files

---
+++ src/components/Facts.svelte
@@ -0,0 +1,16 @@
+<script>
+    export function pageSwitch()
+    {
+        
+    }
+</script>
+
+<style>
+    
+</style>
+<div class="flex-container">
+<p>content</p>
+</div>
+<div class="flex-item">
+
+</div>
