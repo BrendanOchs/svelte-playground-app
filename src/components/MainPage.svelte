e021d2e24c824389b79c61d1062570c8d08f2d6c
Avdo Mesanovic <avdomesanovic@yahoo.com>
2021-02-02 13:24:14 -0600

    transfer to my branch

--- src/components/MainPage.svelte
+++ src/components/MainPage.svelte
@@ -1,5 +1,70 @@
 <div class="flex-container">
-    <div class="flex-item">1</div>
-    <div class="flex-item center">2</div>
-    <div class="flex-item">3</div>
-</div>
+    {#each shoes as shoe}
+    <div class="flex-item text-center">
+        <h4>{shoe.brand}</h4>
+        <p>{shoe.color}</p>
+        <h6>"{shoe.material}"</h6>
+        <button href="">See Details</button>
+    </div>
+    {/each}
+  </div>
+  
+  <script>
+      let shoes = [{
+    "brand": "Goodliffe",
+    "color": "Maroon",
+    "material": "CLS-Class",
+  }, {
+    "brand": "Malamore",
+    "color": "Green",
+    "material": "Corvette",
+  }, {
+    "brand": "Tench",
+    "color": "Puce",
+    "material": "62",
+  }, {
+    "brand": "Runnett",
+    "color": "Crimson",
+    "material": "Tempo",
+  }, {
+    "brand": "Nafziger",
+    "color": "Orange",
+    "material": "300",
+  }, {
+    "brand": "Culbert",
+    "color": "Red",
+    "material": "X5",
+  }, {
+    "brand": "Hudleston",
+    "color": "Turquoise",
+    "material": "Prelude",
+  }, {
+    "brand": "Escolme",
+    "color": "Goldenrod",
+    "material": "Mighty Max Macro",
+  }, {
+    "brand": "Duffett",
+    "color": "Puce",
+    "material": "Jimmy",
+  }, {
+    "brand": "Martyntsev",
+    "color": "Green",
+    "material": "Caravan",
+  }, {
+    "brand": "Gibling",
+    "color": "Pink",
+    "material": "T100 Xtra",
+  }, {
+    "brand": "Cunnah",
+    "color": "Khaki",
+    "material": "M3",
+  }, {
+    "brand": "Olexa",
+    "color": "Yellow",
+    "material": "CR-V",
+  }, {
+    "brand": "Orwin",
+    "color": "Indigo",
+    "material": "Ram 1500",
+  }]
+  </script>
