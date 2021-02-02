b84d671e0c77580c178d6566df8d9d6bbf72f562
Avdo Mesanovic <avdomesanovic@yahoo.com>
2021-02-02 12:16:31 -0600

    added media screen and fake data from mockaroo

--- src/components/MainPage.svelte
+++ src/components/MainPage.svelte
@@ -1,5 +1,84 @@
 <div class="flex-container">
-    <div class="flex-item left">1</div>
+    <div class="flex-item">1</div>
     <div class="flex-item center">2</div>
-    <div class="flex-item right">3</div>
+    <div class="flex-item">3</div>
 </div>
+
+<script>
+    let shoes = [{
+  "brand": "Goodliffe",
+  "color": "Maroon",
+  "material": "CLS-Class",
+  "size": 8
+}, {
+  "brand": "Malamore",
+  "color": "Green",
+  "material": "Corvette",
+  "size": 7
+}, {
+  "brand": "Tench",
+  "color": "Puce",
+  "material": "62",
+  "size": 10
+}, {
+  "brand": "Runnett",
+  "color": "Crimson",
+  "material": "Tempo",
+  "size": 7
+}, {
+  "brand": "Nafziger",
+  "color": "Orange",
+  "material": "300",
+  "size": 7
+}, {
+  "brand": "Culbert",
+  "color": "Red",
+  "material": "X5",
+  "size": 11
+}, {
+  "brand": "Hudleston",
+  "color": "Turquoise",
+  "material": "Prelude",
+  "size": 6
+}, {
+  "brand": "Escolme",
+  "color": "Goldenrod",
+  "material": "Mighty Max Macro",
+  "size": 7
+}, {
+  "brand": "Duffett",
+  "color": "Puce",
+  "material": "Jimmy",
+  "size": 8
+}, {
+  "brand": "Martyntsev",
+  "color": "Green",
+  "material": "Caravan",
+  "size": 8
+}, {
+  "brand": "Gibling",
+  "color": "Pink",
+  "material": "T100 Xtra",
+  "size": 10
+}, {
+  "brand": "Cunnah",
+  "color": "Khaki",
+  "material": "M3",
+  "size": 8
+}, {
+  "brand": "Olexa",
+  "color": "Yellow",
+  "material": "CR-V",
+  "size": 6
+}, {
+  "brand": "Orwin",
+  "color": "Indigo",
+  "material": "Ram 1500",
+  "size": 8
+}, {
+  "brand": "Shera",
+  "color": "Purple",
+  "material": "TSX",
+  "size": 6
+}]
+</script>
