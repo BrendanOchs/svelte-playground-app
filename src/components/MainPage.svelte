79b26c115519a9d717532bb476071df74b0b3f9e
Avdo Mesanovic <avdomesanovic@yahoo.com>
2021-02-09 19:41:57 -0600

    moving things

--- src/components/MainPage.svelte
+++ src/components/MainPage.svelte
@@ -1,52 +1,7 @@
 <section>
-  <div>
-    <DataTable table$aria-label="Shoes"></DataTable>
-    <h2>Data Table</h2>
-      <Head>
-        <Row>
-          <Cell><h3>Brand</h3></Cell>
-          <Cell><h3>Color</h3></Cell>
-          <Cell><h3>Material</h3></Cell>
-        </Row>
-      </Head>
-      <Body>
-          {#each shoes as shoe}
-            <Row>
-              <Cell>{shoe.brand}</Cell>
-              <Cell><p>{shoe.color}</p></Cell>
-              <Cell><p>{shoe.material}</p></Cell>
-            </Row>
-          {/each}
-      </Body>
-    </div>
+  <div class="flex-container">
+    <div class="flex-item">1</div>
+    <div class="flex-item center">2</div>
+    <div class="flex-item">3</div>
+  </div>
 </section>
-  
-  <script>
-    import DataTable, {Head, Body, Row, Cell} from '@smui/data-table';
-
-      let shoes = [{
-    "brand": "Nike",
-    "color": "Red",
-    "material": "Flyknit",
-  }, {
-    "brand": "Adidas",
-    "color": "Green",
-    "material": "Leather",
-  }, {
-    "brand": "Vans",
-    "color": "Black",
-    "material": "Synthetic Leather",
-  }, {
-    "brand": "Sperry",
-    "color": "Brown",
-    "material": "Cardboard",
-  }, {
-    "brand": "Under Armour",
-    "color": "Orange",
-    "material": "Synthetic Leather",
-  }, {
-    "brand": "UGG",
-    "color": "White",
-    "material": "Fur",
-  }]
-  </script>
---
+++ src/routes/_Table.svelte
@@ -0,0 +1,52 @@
+<section>
+    <div>
+      <DataTable table$aria-label="Shoes"></DataTable>
+      <h2>Data Table</h2>
+        <Head>
+          <Row>
+            <Cell><h3>Brand</h3></Cell>
+            <Cell><h3>Color</h3></Cell>
+            <Cell><h3>Material</h3></Cell>
+          </Row>
+        </Head>
+        <Body>
+            {#each shoes as shoe}
+              <Row>
+                <Cell>{shoe.brand}</Cell>
+                <Cell><p>{shoe.color}</p></Cell>
+                <Cell><p>{shoe.material}</p></Cell>
+              </Row>
+            {/each}
+        </Body>
+      </div>
+  </section>
+    
+    <script>
+      import DataTable, {Head, Body, Row, Cell} from '@smui/data-table';
+  
+        let shoes = [{
+      "brand": "Nike",
+      "color": "Red",
+      "material": "Flyknit",
+    }, {
+      "brand": "Adidas",
+      "color": "Green",
+      "material": "Leather",
+    }, {
+      "brand": "Vans",
+      "color": "Black",
+      "material": "Synthetic Leather",
+    }, {
+      "brand": "Sperry",
+      "color": "Brown",
+      "material": "Cardboard",
+    }, {
+      "brand": "Under Armour",
+      "color": "Orange",
+      "material": "Synthetic Leather",
+    }, {
+      "brand": "UGG",
+      "color": "White",
+      "material": "Fur",
+    }]
+    </script>
