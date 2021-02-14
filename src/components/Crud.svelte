5bf557b70adc552c64057350d51da4126976b402
Avdo Mesanovic <avdomesanovic@yahoo.com>
2021-02-13 21:59:54 -0600

    detail view

---
+++ src/components/Crud.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Button, {Label} from 'smui/button';
+    import Textfield from '@smui/textfield';
+    import List, {Item, Text, PrimaryText, SecondaryText} from '@smui/list';
+</script>
+
+<style>
+    
+</style>
+
+<div class="split">
+
+</div>
