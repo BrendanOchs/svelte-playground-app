ac69e63e93dff3c15b3e7d60e2bf51662c3bd7b5
Logan Wyas <loganw9999@gmail.com>
2021-02-19 21:34:09 -0600

    Bug fixes

--- src/routes/inventory/index.svelte
+++ src/routes/inventory/index.svelte
@@ -1,114 +1,50 @@
 <script>
     import List, {Item, Text, PrimaryText, SecondaryText} from '@smui/list';
-    import Textfield from '@smui/textfield';
-    import Button from '@smui/button';
+    import { stores, goto } from '@sapper/app';
+    const { page } = stores();
     import movies from './_movies.js';
-    let movs = [...movies]
-    let filter = "";
-    $: selectedMovie = movs.includes(selectedMovie) ? selectedMovie : movs[0];
-    $: filtered = filter ? movs.filter(mov => {
-        return mov.name.toLowerCase().includes(filter.toLowerCase())
-    }) : movs;
-    $: name = "";
-    $: genre = "";
-    $: desc = "";
-    $: slogan = "";
-    $: reset(selectedMovie);
 
-    function reset(movie) {
-        name = movie ? movie.name : "";
-        genre = movie ? movie.genre : "";
-        desc = movie ? movie.desc : "";
-        slogan = movie ? movie.slogan : "";
+    let selectedIndex = 0;
+    changeWindow(0);
+
+    async function changeWindow(index) {
+        selectedIndex = index;
+        editUrl(index);
     }
 
-    function create() {
-        let id;
-        if (movs.length === 0) {
-            id = 0;
-        }
-        else {
-            id = movs[movs.length - 1].id + 1
-        }
-        movs = [...movs,
-            {
-                name,
-                genre,
-                desc,
-                slogan,
-                id
-            }
-        ]
-        selectedMovie = movs[movs.length - 1];
-    }
-
-    function update() {
-        let i;
-        movs.forEach((mov, index) => {
-            if (mov.id === selectedMovie.id) {
-                i = index;
-            }
-        })
-        movs[i] = {
-            name,
-            genre,
-            desc,
-            slogan,
-            id: i
-        }
-        selectedMovie = movs[i]
-    }
-
-    function remove() {
-        let i;
-        movs.forEach((mov, index) => {
-            if (mov.id === selectedMovie.id) {
-                i = index;
-            }
-        })
-        movs = [...movs.slice(0, i), ...movs.slice(i+1)];
-        selectedMovie = movs[i-1];
+    function editUrl (index) {
+        let result = movies[index].name.split(" ").join('_');
+        goto(`inventory/${result}`);
     }
 </script>
 
-<style>
+<style type="text/scss">
+    @import "@material/typography/mdc-typography";
     .split {
         display: grid;
         grid-template-columns: 1fr 1fr;
     }
-    .mt-20 {
-        margin-top: 20px;
+    .text-left {
+        text-align: left;
     }
-    .m-10 {
-        margin: 10px;
+    .spacing {
+        margin-left: auto;
+        margin-right: auto;
+        width: 90%;
     }
+    /* .norm-fw {
+        font-weight: 500;
+    } */
 </style>
-
-<div class="split">
+<div>
     <List twoLine singleSelection>
-        {#if filtered}
-            {#each filtered as movie}
-                <Item on:SMUI:action={() => selectedMovie = movie} selected={selectedMovie === movie}>
-                    <Text>
-                        <PrimaryText>{movie.name}</PrimaryText>
-                        <SecondaryText>{movie.genre}</SecondaryText>
-                    </Text>
-                </Item>
-            {/each}
-        {/if}
+        {#each movies as movie, i}
+            <Item on:SMUI:action={() => changeWindow(i)} selected={selectedIndex === i}>
+                <Text>
+                    <PrimaryText>{movie.name}</PrimaryText>
+                    <SecondaryText>{movie.genre}</SecondaryText>
+                </Text>
+            </Item>
+        {/each}
     </List>
-    <div class="m-10">
-        <div>
-            <Textfield bind:value={filter} label="Filter" />
-        </div>
-        <Textfield bind:value={name} label="Name" />
-        <Textfield bind:value={genre} label="Genre" />
-        <Textfield bind:value={slogan} label="Slogan" />
-        <div class="mt-20">
-            <Textfield textarea fullwidth bind:value={desc} label="Description" />
-        </div>
-        <Button on:click={() => create()}>Create</Button>
-        <Button on:click={() => update()}>Update</Button>
-        <Button on:click={() => remove()}>Delete</Button>
-    </div>
 </div>
