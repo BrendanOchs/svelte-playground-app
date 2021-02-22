817ab67e6580c20d94a5b956c60c2696499785d9
Alex Szczesiul <szczesiula@stlcaps.com>
2021-02-22 13:49:08 -0600

    updated the user input fields and table

--- src/components/Crud.svelte
+++ src/components/Crud.svelte
@@ -5,6 +5,7 @@
     import DataTable, { Head, Body, Row, Cell } from "@smui/data-table";
     import ProgressBar from "./ProgressBar.svelte";
     import Modal from "./Modal.svelte"
+import { bind } from "svelte/internal";
   
     let deals = [
       {
@@ -12,19 +13,21 @@
         lastName: "Doe",
         email: "exampleEmail@emails.com",
         phoneNum: "(555)555-5555",
+        homeAddress: "10001 Street ave"
       },
     ];
   
     let prefix = '';
-      let firstName = '';
+    let firstName = '';
     let lastName = '';
     let email = '';
     let phoneNum = '';
-      let i = 0;	
+    let homeAddress = '';
+    let i = 0;	
     
     $: filteredDeals = prefix
           ? deals.filter(deals => {
-              const name = `${deals.firstName}, ${deals.lastName}, ${deals.email}, ${deals.phoneNum}`;
+              const name = `${deals.firstName}, ${deals.lastName}, ${deals.email}, ${deals.phoneNum}, ${deals.homeAddress}`;
               return name.toLowerCase().startsWith(prefix.toLowerCase());
           })
           : deals;
@@ -34,27 +37,28 @@
       $: reset_inputs(selected);
   
       function create() {
-          deals = deals.concat({ firstName, lastName, email, phoneNum });
+          deals = deals.concat({ firstName, lastName, email, phoneNum, homeAddress });
           i = deals.length - 1;
-          firstName = lastName = email = phoneNum = '';
+          firstName = lastName = email = phoneNum = homeAddress = '';
       }
   
       function update() {
-          deals[i] = { firstName, lastName, email, phoneNum};
+          deals[i] = { firstName, lastName, email, phoneNum, homeAddress};
       }
   
       function remove() {
           deals = [...deals.slice(0, i), ...deals.slice(i + 1)];
   
-          firstName = lastName = email = phoneNum= '';
+          firstName = lastName = email = phoneNum = homeAddress = '';
           i = Math.min(i, deals.length - 1);
       }
   
       function reset_inputs(deals) {
-          firstName = deals ? deals.firstName : '';
-      lastName = deals ? deals.lastName : '';
-      email = deals ? deals.email : '';
-          phoneNum = deals ? deals.phoneNum : '';
+        firstName = deals ? deals.firstName : '';
+        lastName = deals ? deals.lastName : '';
+        email = deals ? deals.email : '';
+        phoneNum = deals ? deals.phoneNum : '';
+        homeAddress = deals? deals.homeAddress : '';
       }
   
   </script>
@@ -79,16 +83,31 @@
       .buttons {
           clear: both;
       }
+
+      .Inputs{
+          margin-left:30px;
+      }
   </style>
-  
-  <Textfield bind:value={firstName} placeholder="First name"/>
-  <Textfield bind:value={lastName} placeholder="Last Name"/>
-  <Textfield bind:value={email} placeholder="Email"/>
-  <Textfield bind:value={phoneNum} placeholder="(555)555-5555"/>
+
+<div class="Inputs">
+    <Textfield bind:value={firstName} placeholder="First name"/>
+</div>
+<div class="Inputs">
+    <Textfield bind:value={lastName} placeholder="Last Name"/>
+</div>
+<div class="Inputs">
+    <Textfield bind:value={email} placeholder="Email"/>
+</div>
+<div class="Inputs">
+    <Textfield bind:value={phoneNum} placeholder="(555)555-5555"/>
+</div>
+<div class="Inputs">
+    <Textfield bind:value={homeAddress} placeholder="10001 street"/>
+</div>
   
   <div class='buttons'>
-      <Button on:click={create} disabled="{!firstName || !lastName || !email ||!phoneNum}">create</Button>
-      <Button on:click={update} disabled="{!firstName || !lastName || !email ||!phoneNum}">update</Button>
+      <Button on:click={create} disabled="{!firstName || !lastName || !email ||!phoneNum || !homeAddress}">create</Button>
+      <Button on:click={update} disabled="{!firstName || !lastName || !email ||!phoneNum || !homeAddress}">update</Button>
       <Button on:click={remove} disabled="{!selected}">delete</Button>
   </div>
   <body>
@@ -108,6 +127,7 @@
                 <Cell>{item.lastName}</Cell>
                 <Cell>{item.email}</Cell>
                 <Cell>{item.phoneNum}</Cell>
+                <Cell>{item.homeAddress}</Cell>
             </Row>
           {/each}
         </Body>
