b8ddea28f26986324b98755bb98c3c8218d643b1
Logan Wyas <loganw9999@gmail.com>
2021-02-24 13:58:12 -0600

    Formatting changes

--- src/components/OffersDialog.svelte
+++ src/components/OffersDialog.svelte
@@ -1,10 +1,26 @@
 <script>
     import Dialog, {Title, Content, Actions} from '@smui/dialog';
+    import Select, {Option} from '@smui/select';
+    import Icon from '@smui/select/icon/index';
     import Button from '@smui/button';
     import Textfield from '@smui/textfield';
     import { createEventDispatcher } from 'svelte';
 	const dispatch = createEventDispatcher();
     let dialog;
+    let brands = [
+        {
+            name: "Toyota",
+            icon: "https://www.iconsdb.com/icons/preview/soylent-red/toyota-xxl.png"
+        },
+        {
+            name: "Honda",
+            icon: "https://d2dgtayfmpkokn.cloudfront.net/wp-content/uploads/sites/180/2018/06/26130736/cropped-honda-icon-300x300.png"
+        },
+        {
+            name: "Saturn",
+            icon: "https://www.carlogos.org/logo/Saturn-symbol-1985-640x550.jpg"
+        }
+    ]
     export let data = {
         title: "Title",
         content: "Body text",
@@ -39,6 +55,10 @@
     .m-10 {
         margin: 10px;
     }
+    img {
+        height: 100%;
+        margin-right: 10px;
+    }
 </style>
 
 <Dialog bind:this={dialog} aria-labelledby="title" aria-describedby="content">
@@ -46,9 +66,16 @@
     <Content id="content">
         {#if data.reason != "delete"}
             <div class="split">
-                <span class="m-10"><Textfield bind:value={data.car.brand} label="Brand" /></span>
+                <!-- <span class="m-10"><Textfield bind:value={data.car.brand} label="Brand" /></span> -->
+                <span class="m-10">
+                    <Select bind:value={data.car.brand} label="Brand">
+                        {#each brands as brand}
+                            <Option value={brand.name}><img src={brand.icon} alt="{brand.name} Icon">{brand.name}</Option>
+                        {/each}
+                    </Select>
+                </span>
                 <span class="m-10"><Textfield bind:value={data.car.model} label="Model" /></span>
-                <span class="m-10"><Textfield type="number" input$step="1" input$min="1900" input$max="2021" bind:value={data.car.year} label="Year" /></span>
+                <span class="m-10"><Textfield type="number" input$step="1" input$min="1900" input$max="2100" bind:value={data.car.year} label="Year" /></span>
                 <span class="m-10"><Textfield bind:value={data.car.color} label="Color" /></span>
                 <span class="m-10"><Textfield type="number" input$step="1000" input$min="0" bind:value={data.car.price} label="Price" /></span>
                 <span class="m-10"><Textfield type="url" bind:value={data.car.img} label="Link" /></span>
