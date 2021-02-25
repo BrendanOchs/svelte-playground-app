6d1e0342f1252e92d566fc1b31b9f8e3b31bdc4f
romina1127 <diaz-riveror@stlcaps.net>
2021-02-25 13:31:51 -0600

    trying to make slide function for faq

--- src/routes/faq.svelte
+++ src/routes/faq.svelte
@@ -12,17 +12,32 @@
 </svelte:head>
 
 <script>
+    import Card, {Content, PrimaryAction, Media, MediaContent, Actions, ActionButtons, ActionIcons} from '@smui/card';
+    import IconButton, {Icon} from '@smui/icon-button';
+    import Button, {Label} from '@smui/button';
+    import {slide} from 'svelte/transition';
 
+    let pressed = false;
 </script>
 
 <style>
     .heading {
         margin-left: 10px;
     }
-    .container {
-        margin-left: 100px;
-        margin-right: 100px;
+    .flex-container.faq {
+        margin: 0 auto;
         border: 1px solid black;
+        flex-direction: column;
+    }
+
+    .flex-item {
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        color: red;
+        background-color: #f8f8f8;
+        margin-right: 20px;
+        margin-bottom: 20px;
     }
 </style>
 
@@ -31,8 +46,36 @@
     <p>Here are the questions we are asked the most.</p>
 </div>
 
+<div class="flex-container faq">
+    <div class="flex-item">
+        <Card style="width: 100%" padded>
+            <Content>Lorem ipsum dolor sit amet, consectetur adipiscing elit?<IconButton class="material-icons" on:click={() =>  pressed = !pressed}>add</IconButton></Content>
+            {#if pressed == true}
+                <div transition:slide>
+                    <p>Eu sem integer vitae justo eget magna.</p>
+                </div>
+            {/if}
+        </Card>
+    </div>
+    <div class="flex-item">
+        <Card padded>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?
+            <p>1. Vitae justo eget magna</p>
+            <p>2. Adipiscing elit sed</p>
+            <p>3. Eu sem integer</p>
+        </Card>
+    </div>
+    <div class="flex-item">
+        <Card padded>
+
+        </Card>
+    </div>
+    
+    
+</div>
+
 <div class="container">
-    <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?</h4>
+    <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit?</h4>
     <p>Eu sem integer vitae justo eget magna.</p>
     <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?</h4>
     <p>1. Vitae justo eget magna</p>
