50d3649c7fd3d2fa64945365e9f6a01dd5193afa
Avdo Mesanovic <avdomesanovic@yahoo.com>
2021-02-25 12:49:18 -0600

    added bios to about, functioning but needs work

---
+++ src/components/UserDetail.svelte
@@ -0,0 +1,48 @@
+<script>
+    export let user;
+    let showProfile=false;
+    const showBio=() => showProfile = !showProfile;
+</script>
+ 
+<style>
+    .dropbtn {
+        margin-left: 205px;
+    }
+    .dropbtn:hover{
+        background-color: yellow;
+    }
+    .dropdown {
+        position: relative;
+        display: inline-block;
+    }
+    .container {
+        border: 1px solid black
+    }
+    h5{
+        text-align: center;
+        margin: 0 auto;
+    }
+    p {
+        text-align: center;
+        margin: 0 auto;
+    }
+    .img-responsive {
+        border-radius: 50%;
+        width: 100%;
+        max-width: 400px;
+        margin: 0 0 1em 0;
+    }
+    .showProfile {
+        display:none;
+    }
+</style>
+ 
+<div class="container">
+    <img class="img-responsive" src="person.jpg" alt="placeholder"> 
+    <h5>{user.name}</h5>
+    <p>{user.role}</p>
+    <div class="dropdown">
+    <button on:click="{showBio}" class="dropbtn">Show Bio</button>
+    <p class:showProfile={showProfile}>{user.bio}</p>
+    </div>
+</div>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -12,42 +12,30 @@
 </svelte:head>
 
 <style>
-	.flex-container {
-		border: 1px dashed red;
-		display: flex;
-		padding: 0;
-		margin: 0;
-		flex-direction: row;
-	}
 
-	.flex-item {
-		border: 1px dashed grey;
-		padding: 5px;
-		margin-top: 10px;
-		margin: 5px;
-	}
-
-	.flex-item.video {
-		width: 550px;
+	.container {
+		border: 1px dashed gold;
+		text-align: center;
 	}
 
 	h1 {
 		text-align: center;
+	}
+	.flex-container {
+		justify-content: center;
 	}
 
 </style>
 
 <h1>About this site</h1>
 
-<div class="flex-container about">
-	<div class="flex-item description">
+<div class="container">
 		<p>
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor nulla tincidunt sem dignissim sagittis.
 			Mauris non laoreet lacus, ac facilisis nulla. Sed faucibus sodales venenatis. Nunc mauris nisl, tempus sit
 			amet metus vel, condimentum tempor enim. Pellentesque sit amet mi mi. Cras aliquam mollis risus, a luctus
 			sem facilisis nec. Curabitur bibendum eu odio sed elementum. Mauris pellentesque feugiat orci in 
-			pellentesque. Nulla sodales, dolor eget porttitor accumsan, turpis elit efficitur nulla, eu consectetur
-			nibh odio id ligula.
+			pellentesque. Nulla sodales, dolor eget.
 			<br>
 			Integer eget volutpat elit, convallis iaculis mi. Suspendisse id dui et sem placerat vulputate in non tellus.
 			Morbi auctor sagittis nisl vel convallis. Proin venenatis quam lacinia, vulputate metus condimentum, 
@@ -56,56 +44,33 @@
 			feugiat ligula auctor. Nullam vel odio nisi. Sed accumsan velit nec purus fermentum, et viverra velit tincidunt. 
 			Suspendisse faucibus nisl sem, ac vulputate sapien accumsan sit amet. Quisque a sodales arcu, sed vestibulum sem.
 		</p>
-	</div>
-	<div class="flex-item video">
-		<!-- <video poster="https://sveltejs.github.io/assets/caminandes-llamigos.jpg" src="https://www.youtube.com/watch?v=zakwgiLakoc"
-		on:mousemove={handleMousemove}
-		on:mousedown={handleMousedown}
-		bind:duration
-		bind:paused><track kind="captions"></video> -->
-			Video
-	</div>
 </div>
 
+<div class="flex-container">
+    {#each users as userdetails}
+        <div class="flex-item">
+            <UserDetail user="{userdetails}"></UserDetail>
+        </div>  
+    {/each}
+</div>
+ 
 <script>
-	// These values are bound to properties of the video
-	let time = 0;
-	let duration;
-	let paused = true;
-
-	let showControls = true;
-	let showControlsTimeout;
-
-	function handleMousemove(e) {
-		// Make the controls visible, but fade out after
-		// 2.5 seconds of inactivity
-		clearTimeout(showControlsTimeout);
-		showControlsTimeout = setTimeout(() => showControls = false, 2500);
-		showControls = true;
-
-		if (!(e.buttons & 1)) return; // mouse not down
-		if (!duration) return; // video not loaded yet
-
-		const { left, right } = this.getBoundingClientRect();
-		time = duration * (e.clientX - left) / (right - left);
-	}
-
-	function handleMousedown(e) {
-		// we can't rely on the built-in click event, because it fires
-		// after a drag — we have to listen for clicks ourselves
-
-		function handleMouseup() {
-			if (paused) e.target.play();
-			else e.target.pause();
-			cancel();
-		}
-
-		function cancel() {
-			e.target.removeEventListener('mouseup', handleMouseup);
-		}
-
-		e.target.addEventListener('mouseup', handleMouseup);
-
-		setTimeout(cancel, 200);
-	}
+    import UserDetail from '../components/UserDetail.svelte'
+    const users = [
+        {
+            name: 'Oliver Kingston',
+            role: 'Lead Developer',
+            bio: 'Oliver is our lead developer at the company. He has been with us for 10+ years and has really helped the company out.'
+        }, 
+        {
+            name: 'Danica Rosenburg',
+            role: 'Marketing Executive',
+            bio: 'Danica is our superstar marketing executive. She has been with us for 2 years, and it wouldn\'t be the same without her.'
+        },  
+        {
+            name: 'Abdul Solomon',
+            role: 'Network Administrator',
+            bio: 'Abdul is the company\'s network administrator. He has just started this year and we are excited to see what he can do.'
+        }
+    ]
 </script>
